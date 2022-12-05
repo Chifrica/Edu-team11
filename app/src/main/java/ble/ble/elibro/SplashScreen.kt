345d64ebe62b9654d1204ea51f67a3a57cafccf7
AndroQues <chikaonwunali2019@gmail.com>
2022-12-05 06:45:27 +0100

    git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/Chifrica/Edu-team11.git git push -u origin main

---
+++ app/src/main/java/ble/ble/elibro/SplashScreen.kt
@@ -0,0 +1,27 @@
+package ble.ble.elibro
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.fragment.app.Fragment
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler (Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
